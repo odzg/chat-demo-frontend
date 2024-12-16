103a41ebf5259af6e85b7401120b30335dba8cca
Oded Zilbershlag <123odzil@gmail.com>
2024-12-16 17:00:18 +0200

    Updated dependencies & minor improvements to `eslint.config.js`

--- eslint.config.js
+++ eslint.config.js
@@ -29,7 +29,10 @@ import tseslint from 'typescript-eslint';
 
 const JS_EXTENSIONS_GLOB = '**/*.{js,cjs}';
 const TS_EXTENSIONS_GLOB = '**/*.{ts,tsx}';
-const JS_TS_EXTENSION_GLOBS = [JS_EXTENSIONS_GLOB, TS_EXTENSIONS_GLOB];
+const JS_TS_EXTENSION_GLOBS = /** @type {const} **/ ([
+  JS_EXTENSIONS_GLOB,
+  TS_EXTENSIONS_GLOB,
+]);
 
 export default typegen([
   {
@@ -51,7 +54,7 @@ export default typegen([
       ...tseslint.configs.strictTypeChecked,
       ...tseslint.configs.stylisticTypeChecked,
     ],
-    files: JS_TS_EXTENSION_GLOBS,
+    files: [...JS_TS_EXTENSION_GLOBS],
     languageOptions: {
       parserOptions: {
         projectService: true,
@@ -73,6 +76,12 @@ export default typegen([
       '@typescript-eslint/consistent-type-imports': [
         'error',
         { fixStyle: 'inline-type-imports' },
+      ],
+      '@typescript-eslint/no-empty-object-type': [
+        'error',
+        {
+          allowInterfaces: 'with-single-extends',
+        },
       ],
       '@typescript-eslint/no-import-type-side-effects': 'error',
       '@typescript-eslint/no-unused-vars': [
