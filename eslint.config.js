43490c76509bc33ccbeb1e3a5c3e4643667047d3
Oded Zilbershlag <123odzil@gmail.com>
2025-04-23 02:26:50 +0300

    refactor(eslint): remove redundant "tseslint.configs.recommendedTypeChecked"

--- eslint.config.js
+++ eslint.config.js
@@ -50,7 +50,6 @@ export default typegen(
     js.configs.recommended,
     {
       extends: [
-        tseslint.configs.recommendedTypeChecked,
         tseslint.configs.strictTypeChecked,
         tseslint.configs.stylisticTypeChecked,
         react.configs['recommended-type-checked'],
